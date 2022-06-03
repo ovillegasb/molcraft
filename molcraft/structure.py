0d2d6d8f91319c4b3bdf33ad9b589b4e37ae581a
Orlando Villegas <ragnar0317@gmail.com>
2022-06-03 14:44:18 +0200

    Start with the construction of the molcraft package module

---
+++ molcraft/structure.py
@@ -0,0 +1,8 @@
+"""
+The submodule structure contains the main molcraft classes.
+
+It is created to work with input and output files. It is also used to extract
+information from a molecular system. It defines the three main classes: BULK,
+MOL and ATOM.
+
+"""
