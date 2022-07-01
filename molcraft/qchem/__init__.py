624da537a81d0c6950c6325f749d63351abee039
Orlando Villegas <ragnar0317@gmail.com>
2022-07-01 16:02:34 +0200

    new sub module for quamtum work

---
+++ molcraft/qchem/__init__.py
@@ -0,0 +1,11 @@
+"""
+QChem - Quantum Chemistry Toolkit.
+
+It is a submodule of MolCraft that compiles tools to perform quantum
+calculations, these calculations are focused on geometrical optimization and
+partial charges.
+
+Reference:
+https://nznano.blogspot.com/2018/03/simple-quantum-chemistry-hartree-fock.html
+
+"""
