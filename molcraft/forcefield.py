fb793a0ea188a44282e85be12b7516741e864f37
Orlando Villegas <ragnar0317@gmail.com>
2022-10-07 19:07:28 +0200

    New submodule forcefield

---
+++ molcraft/forcefield.py
@@ -0,0 +1,18 @@
+"""Module that compiles the classes and functions to work with force fields."""
+
+
+"""
+- Objeto campo de fuerzas.
+- Se iniciara con el campo de fuerzas OPLS-AA
+- Interface que conecte con BOSS (cargas 1.14 CM1)
+- El objeto cargara los parametros del campo de fuerzas
+de una molecula.
+- Tendra la capacidad de guardar en los formatos necesarios.
+
+"""
+
+
+class FField:
+    """Class representing the chosen force field."""
+
+    pass
